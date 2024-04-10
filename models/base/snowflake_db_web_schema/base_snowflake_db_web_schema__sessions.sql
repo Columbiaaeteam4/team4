fcb195d86dec9ee6abe327b51a00be821031bab9
Hanyang Cao <hc3417@columbia.edu>
2024-04-10 03:25:53 +0000

    build base model from snowflake

---
+++ models/base/snowflake_db_web_schema/base_snowflake_db_web_schema__sessions.sql
@@ -0,0 +1,10 @@
+select
+_FIVETRAN_ID, 
+CAST(CLIENT_ID AS STRING) AS CLIENT_ID, 
+IP, 
+SESSION_ID, 
+SESSION_AT AS SESSION_AT_TS, 
+OS, 
+_FIVETRAN_DELETED, 
+_FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_TS
+from {{  source('snowflake_db_web_schema', 'sessions')}}
